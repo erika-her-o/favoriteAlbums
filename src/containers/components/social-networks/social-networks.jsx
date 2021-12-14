b346c507bbaf4611541df763b1d8b35b046ee2e2
Erika Hernández <74832999+erika-her-o@users.noreply.github.com>
2021-12-14 18:10:20 -0300

    add icon social-networks

--- src/containers/components/social-networks/social-networks.jsx
+++ src/containers/components/social-networks/social-networks.jsx
@@ -4,18 +4,14 @@ export default function SocialNetworks() {
   return (
     <>
       <div>
-          <h1>redes</h1>
+          <h1>Redes</h1>
 
-          <div class="container footer-content">
-            <div class="social-media">
-              
-                <a href="./" target="_blank">
-                    <img src="https://img.icons8.com/plasticine/100/000000/facebook.png"/>
-                </a>
-              
+          <div className="container-social">
+            <div className="social-media">
+                <img src="https://img.icons8.com/bubbles/300/000000/facebook.png"/>
+                <img src="https://img.icons8.com/bubbles/300/000000/instagram-new--v2.png"/>
+                <img src="https://img.icons8.com/bubbles/300/000000/tiktok.png"/>
             </div>
-
-        
         </div>
       </div>
     </>
